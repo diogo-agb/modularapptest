948dd394b7dcd833f2039c519829f484dc74d572
Diogo <diogo.agb@gmail.com>
2021-07-10 15:46:57 -0300

    file build.yaml

--- lib/app/modules/yellow/yellow_store.dart
+++ lib/app/modules/yellow/yellow_store.dart
@@ -3,13 +3,13 @@ import 'package:mobx/mobx.dart';
 part 'yellow_store.g.dart';
 
 class YellowStore = _YellowStoreBase with _$YellowStore;
-abstract class _YellowStoreBase with Store {
 
+abstract class _YellowStoreBase with Store {
   @observable
   int value = 0;
 
   @action
   void increment() {
     value++;
-  } 
+  }
 }
